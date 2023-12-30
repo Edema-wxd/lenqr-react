87e919ae6fe8d58c9903196008d1332032725613
Woods Francis <100766884+Edema-wxd@users.noreply.github.com>
2023-12-30 16:09:09 +0100

    styling and building the Signin section

--- src/pages/SignIn/Signin.jsx
+++ src/pages/SignIn/Signin.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Stack, Container, TextField, Button } from "@mui/material";
+import {useNavigate} from "react-router-dom"
 
 function Signin() {
+    const navigate  = useNavigate()
   return (
-    <div>Signin</div>
-  )
+    <Container>
+      <Stack direction="row" justifyContent="center" alignItems="center">
+        <img src="" alt="" />
+        <div>
+          <h2>Welcome</h2>
+          <p>Enter details to login.</p>
+          <form action="">
+            <Stack>
+              <TextField
+                fullWidth
+                type="email"
+                id="email"
+                placeholder="Email"
+              />
+              <TextField
+                fullWidth
+                type="password"
+                id="password"
+                placeholder="Password"
+              />
+              <Button variant="text" style={{ textTransform: "capitalize" }}>
+                forgot password?
+              </Button>
+              <Button
+                variant="contained"
+                style={{ textTransform: "capitalize" }}
+                size="large"
+                onClick={navigate("dashboard/main")}
+              >
+                Submit
+              </Button>
+            </Stack>
+          </form>
+        </div>
+      </Stack>
+    </Container>
+  );
 }
 
-export default Signin
+export default Signin;
